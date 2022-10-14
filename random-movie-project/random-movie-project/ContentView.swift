083a496d0afd174271bac2a04e5c058d023f3ead
Sammy Kay <sammyzkay@gmail.com>
2022-10-14 14:10:34 -0400

    initial push

---
+++ random-movie-project/random-movie-project/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  random-movie-project
+//
+//  Created by Sammy Kay on 10/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ random-movie-project/random-movie-project/random_movie_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  random_movie_projectApp.swift
+//  random-movie-project
+//
+//  Created by Sammy Kay on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct random_movie_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
